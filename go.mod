37a492e4d1d8c630c23524cd4471ed764eb71915
Yuno <prcups@163.com>
2021-07-09 01:33:36 +0800

    new config loader

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ChimataMS
+
+go 1.16
+
+require github.com/pelletier/go-toml/v2 v2.0.0-beta.3
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "ChimataMS/worker"
+
+func main() {
+  worker.LoadConfig()
+}
