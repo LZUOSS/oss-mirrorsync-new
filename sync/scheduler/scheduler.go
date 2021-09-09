011df176631b310a003c026522cf943fec1ab893
Yuno <prcups@163.com>
2021-09-09 12:22:59 +0800

    Init Scheduler: asynchronous support

--- sync/scheduler/scheduler.go
+++ sync/scheduler/scheduler.go
@@ -1,66 +1,79 @@
 package scheduler
 
 import (
-  "ChimataMS/worker"
-  "github.com/robfig/cron"
-  "log"
-  "os"
-  "os/exec"
-  "path/filepath"
-  "time"
+	"ChimataMS/worker"
+	"github.com/robfig/cron"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"time"
 )
 
 type mirrorStruct struct {
-  Config *worker.MirrorConfigStruct
-  Channel chan int
-  SyncStatus string
-  LastSyncTime string
+	Config       *worker.MirrorConfigStruct
+	Channel      chan int
+	SyncStatus   string
+	LastSyncTime string
 }
 
 var (
-  mirrorMap = make(map[string]*mirrorStruct)
+	mirrorMap = make(map[string]*mirrorStruct)
 )
 
 func (mirror *mirrorStruct) Run() {
-  process := exec.Command(worker.Config.Base.Shell, "-c \"" + mirror.Config.Exec + "\"")
-  process.Env = append(process.Env, "PUBLIC_PATH=" + worker.Config.Base.PublicPath)
-  process.Dir = worker.Config.Base.PublicPath
-  process.Stdout = worker.LogFile
-  process.Stderr = worker.LogFile
-  process.Run()
+	process := exec.Command(worker.Config.Base.Shell, "-c \""+mirror.Config.Exec+"\"")
+	process.Env = append(process.Env, "PUBLIC_PATH="+filepath.Join(worker.Config.Base.PublicPath, mirror.Config.Name))
+	process.Dir = worker.Config.Base.PublicPath
+	process.Stdout = worker.LogFile
+	process.Stderr = worker.LogFile
+	process.Run()
 }
 
 var (
-  mirrorCron *cron.Cron
+	mirrorCron *cron.Cron
 )
 
 func InitScheduler() {
-  log.Println("Init scheduler...")
-  mirrorCron = cron.New()
-  worker.ConfigMutex.RLock()
-   for _, mirror := range worker.Config.Mirrors {
-     curMirror := new(mirrorStruct)
-     curMirror.Config = mirror
-     err := mirrorCron.AddJob(mirror.Period, curMirror)
-     if err != nil {
-       log.Println("Cron can't load mirror " + mirror.Name + ".")
-       continue
-     }
-     mirrorMap[mirror.Name] = curMirror
-   }
-   mirrorCron.AddFunc("@daily", func(){
-     worker.ConfigMutex.RLock()
-     err := worker.LogFile.Close()
-     if err != nil {
-       log.Println(err)
-     }
-     worker.LogFile, err = os.Create(filepath.Join(worker.Config.Base.LogPath, time.Now().Local().Format("20060102"), ".log"))
-     if err != nil {
-       log.Println(err)
-     } else {
-       worker.ConfigMutex.RUnlock()
-     }
-   })
-   worker.ConfigMutex.RUnlock()
-   mirrorCron.Run()
+	log.Println("Init scheduler...")
+	mirrorCron = cron.New()
+	worker.ConfigMutex.RLock()
+	for _, mirror := range worker.Config.Mirrors {
+		mirror := mirror
+		go func() {
+			curMirror := new(mirrorStruct)
+			curMirror.Config = mirror
+			init := exec.Command(worker.Config.Base.Shell, "-c \""+mirror.InitExec+"\"")
+			init.Env = append(process.Env, "PUBLIC_PATH="+filepath.Join(worker.Config.Base.PublicPath, mirror.Config.Name))
+			init.Dir = worker.Config.Base.PublicPath
+			init.Stdout = worker.LogFile
+			init.Stderr = worker.LogFile
+			err := init.Run()
+			if err != nil {
+				log.Println("Init failed")
+			} else {
+				err = mirrorCron.AddJob(mirror.Period, curMirror)
+				if err != nil {
+					log.Println("Cron can't load mirror " + mirror.Name + ".")
+					return
+				}
+				mirrorMap[mirror.Name] = curMirror
+			}
+		}()
+	}
+	mirrorCron.AddFunc("@daily", func() {
+		worker.ConfigMutex.RLock()
+		err := worker.LogFile.Close()
+		if err != nil {
+			log.Println(err)
+		}
+		worker.LogFile, err = os.Create(filepath.Join(worker.Config.Base.LogPath, time.Now().Local().Format("20060102"), ".log"))
+		if err != nil {
+			log.Println(err)
+		} else {
+			worker.ConfigMutex.RUnlock()
+		}
+	})
+	worker.ConfigMutex.RUnlock()
+	mirrorCron.Run()
 }
--- sync/worker/worker.go
+++ sync/worker/worker.go
@@ -1,129 +1,129 @@
 package worker
 
 import (
-  "errors"
-  "github.com/pelletier/go-toml/v2"
-  "io/ioutil"
-  "log"
-  "os"
-  "path/filepath"
-  "sync"
-  "time"
+	"errors"
+	"github.com/pelletier/go-toml/v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
 )
 
 type MirrorConfigStruct struct {
-  Name        string `toml:name`
-  PrepareExec string `toml:prepare_exec,multiline:"true"`
-  Exec        string `toml:exec,multiline:"true"`
-  SuccessExec string `toml:success_exec,multiline:"true"`
-  FailExec    string `toml:fail_exec,multiline:"true"`
-  Period      string `toml:period`
+	Name        string `toml:name`
+	InitExec    string `toml:init_exec,multiline:"true"`
+	Exec        string `toml:exec,multiline:"true"`
+	SuccessExec string `toml:success_exec,multiline:"true"`
+	FailExec    string `toml:fail_exec,multiline:"true"`
+	Period      string `toml:period`
 }
 
 type BaseConfigStruct struct {
-  PublicPath       string             `toml:"public_path"`
-  LogPath          string             `toml:"log_path"`
-  MirrorConfigPath string             `toml:"mirror_config_path"`
-  RecordPath       string             `toml:"record_path"`
-  Shell            string             `toml:"shell"`
+	PublicPath       string `toml:"public_path"`
+	LogPath          string `toml:"log_path"`
+	MirrorConfigPath string `toml:"mirror_config_path"`
+	RecordPath       string `toml:"record_path"`
+	Shell            string `toml:"shell"`
 }
 
 type ConfigStruct struct {
-  Base    *BaseConfigStruct           `toml:"base"`
-  Mirrors []*MirrorConfigStruct       `toml:"mirrors"`
+	Base    *BaseConfigStruct     `toml:"base"`
+	Mirrors []*MirrorConfigStruct `toml:"mirrors"`
 }
 
 var (
-  ConfigMutex sync.RWMutex
-  Config      *ConfigStruct
-  LogFile     *os.File
+	ConfigMutex sync.RWMutex
+	Config      *ConfigStruct
+	LogFile     *os.File
 )
 
-func checkConfig() bool{
-  if Config.Base.Shell == "" {
-    Config.Base.Shell = "bash"
-  }
-  var err error
-  Config.Base.PublicPath, err = filepath.Abs(Config.Base.PublicPath)
-  if err != nil {
-    return false
-  }
-  if err != nil {
-    log.Println("The public path is not valid.")
-    return false
-  }
-  Config.Base.MirrorConfigPath, err = filepath.Abs(Config.Base.MirrorConfigPath)
-  if err != nil {
-    log.Println("The mirror config path is not valid.")
-    return false
-  }
-  Config.Base.LogPath, err = filepath.Abs(Config.Base.LogPath)
-  if err != nil {
-    log.Println("The log path is not valid.")
-    return false
-  }
-  Config.Base.RecordPath, err = filepath.Abs(Config.Base.RecordPath)
-  if err != nil {
-    log.Println("The record path is not valid.")
-    return false
-  }
-  return true
+func checkConfig() bool {
+	if Config.Base.Shell == "" {
+		Config.Base.Shell = "bash"
+	}
+	var err error
+	Config.Base.PublicPath, err = filepath.Abs(Config.Base.PublicPath)
+	if err != nil {
+		return false
+	}
+	if err != nil {
+		log.Println("The public path is not valid.")
+		return false
+	}
+	Config.Base.MirrorConfigPath, err = filepath.Abs(Config.Base.MirrorConfigPath)
+	if err != nil {
+		log.Println("The mirror config path is not valid.")
+		return false
+	}
+	Config.Base.LogPath, err = filepath.Abs(Config.Base.LogPath)
+	if err != nil {
+		log.Println("The log path is not valid.")
+		return false
+	}
+	Config.Base.RecordPath, err = filepath.Abs(Config.Base.RecordPath)
+	if err != nil {
+		log.Println("The record path is not valid.")
+		return false
+	}
+	return true
 }
 
 func initLogger() {
-  var err error
-  LogFile, err = os.Create(filepath.Join(Config.Base.LogPath, time.Now().Local().Format("20060102") + ".log"))
-  if err != nil {
-    log.Println(err)
-  } else {
-    log.SetOutput(LogFile)
-  }
+	var err error
+	LogFile, err = os.Create(filepath.Join(Config.Base.LogPath, time.Now().Local().Format("20060102")+".log"))
+	if err != nil {
+		log.Println(err)
+	} else {
+		log.SetOutput(LogFile)
+	}
 
 }
 
 func LoadConfig() {
-  ConfigMutex.Lock()
-  log.Println("Initializing...")
-  Config = new(ConfigStruct)
-  log.Println("Loading config.toml...")
-  baseConfigContent, err := ioutil.ReadFile("./config.toml")
-  if err != nil {
-    if errors.Is(err, os.ErrNotExist) {
-      baseConfigContent, err = ioutil.ReadFile("/etc/Chimata/config.toml")
-      if err != nil {
-        log.Fatal(err)
-      }
-    } else {
-      log.Fatal(err)
-    }
-  }
-  log.Println("Unmarshalling config.toml...")
-  err = toml.Unmarshal(baseConfigContent, Config)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if checkConfig() == false {
-    log.Fatal("The config file has error(s).")
-  }
-  log.Println("Loading mirrors...")
-  mirrorsConfigFile, err := ioutil.ReadDir(Config.Base.MirrorConfigPath)
-  if err != nil {
-    log.Fatal(err)
-  }
-  for _, oneMirrorConfigFile := range mirrorsConfigFile {
-    if len(oneMirrorConfigFile.Name()) > 5 && oneMirrorConfigFile.Name()[len(oneMirrorConfigFile.Name()) - 5 :] == ".toml" {
-      log.Println("Loading " + oneMirrorConfigFile.Name() + "...")
-      oneMirrorConfigContent, err := ioutil.ReadFile(filepath.Join(Config.Base.MirrorConfigPath, oneMirrorConfigFile.Name()))
-      if err != nil {
-        log.Fatal(err)
-      }
-      log.Println("Unmarshalling " + oneMirrorConfigFile.Name() + "...")
-      err = toml.Unmarshal(oneMirrorConfigContent, Config)
-      if err != nil {
-        log.Fatal(err)
-      }
-    }
-  }
-  initLogger()
-  ConfigMutex.Unlock()
+	ConfigMutex.Lock()
+	log.Println("Initializing...")
+	Config = new(ConfigStruct)
+	log.Println("Loading config.toml...")
+	baseConfigContent, err := ioutil.ReadFile("./config.toml")
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			baseConfigContent, err = ioutil.ReadFile("/etc/Chimata/config.toml")
+			if err != nil {
+				log.Fatal(err)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+	log.Println("Unmarshalling config.toml...")
+	err = toml.Unmarshal(baseConfigContent, Config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if checkConfig() == false {
+		log.Fatal("The config file has error(s).")
+	}
+	log.Println("Loading mirrors...")
+	mirrorsConfigFile, err := ioutil.ReadDir(Config.Base.MirrorConfigPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, oneMirrorConfigFile := range mirrorsConfigFile {
+		if len(oneMirrorConfigFile.Name()) > 5 && oneMirrorConfigFile.Name()[len(oneMirrorConfigFile.Name())-5:] == ".toml" {
+			log.Println("Loading " + oneMirrorConfigFile.Name() + "...")
+			oneMirrorConfigContent, err := ioutil.ReadFile(filepath.Join(Config.Base.MirrorConfigPath, oneMirrorConfigFile.Name()))
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println("Unmarshalling " + oneMirrorConfigFile.Name() + "...")
+			err = toml.Unmarshal(oneMirrorConfigContent, Config)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}
+	initLogger()
+	ConfigMutex.Unlock()
 }
